03a8fd662315cdef7d056e63837db9c08ce0025e
jboscan@wedootech.com <joeypc.py@gmail.com>
2020-03-20 12:00:27 -0400

    Actualización de algoritmos

--- Python/Ordenamiento_por_burbuja.py
+++ Python/Ordenamiento_por_burbuja.py
@@ -1,49 +1,24 @@
-# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
-# Es posible que el codigo generado no sea completamente correcto. Si encuentra
-# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+import random
 
-from random import randint
+def intercambiarValores(array, pos1, pos2):
+    temporal = array[pos1]
+    array[pos1] = array[pos2]
+    array[pos2] = temporal
 
-# En python no hay forma de elegir como pasar una variable a una
-# funcion (por referencia o por valor). Las variables "inmutables"
-# (str, int, float, bool) se pasan siempre por copia mientras que
-# las demas (listas, objetos, etc.) se pasan siempre por referencia.
-# Esto coincide con el comportamiento por defecto en pseint, pero
-# cuando utiliza las palabras clave "Por Referencia" para
-# alterarlo la traducción no es correcta.
-
-def intercambiarvalores(arreglo, pos1, pos2):
-	temporal = float()
-	temporal = arreglo[pos1]
-	arreglo[pos1] = arreglo[pos2]
-	arreglo[pos2] = temporal
-	mostrararreglo(arreglo,10)
-
-def ordenamientoporburbuja(arreglo, nelementos):
-	i = float()
-	j = float()
-	intercambiados = bool()
-	while True:# no hay 'repetir' en python
+def ordenamiento(array, size):
+	while True:
 		intercambiados = False
-		for i in range(1,nelementos):
-			if arreglo[i-1]>arreglo[i]:
-				intercambiarvalores(arreglo,i-1,i)
+		for i in range(1, size):
+			if array[i] < array[i-1]:
+				intercambiarValores(array, i, i-1)
 				intercambiados = True
-		if (intercambiados==False): break
+		if intercambiados == False: break
 
-def mostrararreglo(arreglo, nelementos):
-	for i in range(nelementos):
-		print(arreglo[i]," ", end="")
-	print("")
-
-if __name__ == '__main__':
-	arreglo = float()
-	nelementos = float()
-	nelementos = 10
-	arreglo = [int() for ind0 in range(nelementos)]
-	for i in range(nelementos):
-		arreglo[i] = randint(0,99)
-	mostrararreglo(arreglo,nelementos)
-	ordenamientoporburbuja(arreglo,nelementos)
-	mostrararreglo(arreglo,nelementos)
-
+if __name__ == "__main__":
+    array = []
+    size = 10
+    for i in range(size):
+        array.append(random.randint(0, 100))
+    print(array)
+    ordenamiento(array, size)
+    print(array)
